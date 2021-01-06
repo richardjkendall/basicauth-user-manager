8f25ec395b542680744fef8f4b43cebb11124a2f
Richard Kendall <richard.kendall@gmail.com>
2021-01-06 14:31:29 +1100

    another attempt at fix for root redirect

--- app.py
+++ app.py
@@ -61,9 +61,27 @@ else:
 
 @app.route("/")
 def gotoindex():
+  # headers on request
+  logger.info("Headers on request...")
   for key,value in dict(request.headers).items():
     logger.info("{key} -> {val}".format(key=key, val=value))
-  return redirect("/index.html", code=301)
+  # check for X-Forwarded headers
+  if request.headers.get("x-forwarded-host"):
+    host = request.headers.get("x-forwarded-host")
+    # check if host is a list and get first element assuming this is the host the user expects
+    if len(host.split(",")) > 1:
+      host = host.split(",")[0].strip()
+    if request.headers.get("x-forwarded-proto"):
+      proto = request.headers.get("x-forwarded-proto")
+      url = "{proto}://{host}/index.html".format(proto=proto, host=host)
+      logger.info("URL for redirect is {url}".format(url=url))
+      return redirect(url, code=302)
+    else:
+      url = "http://{host}/index.html".format(host=host)
+      logger.info("URL for redirect is {url}".format(url=url))
+      return redirect(url, code=302)
+  else:
+    return redirect("/index.html", code=302)
 
 @app.route("/api")
 def root():
